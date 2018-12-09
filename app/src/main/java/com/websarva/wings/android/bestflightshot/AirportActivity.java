075357f2ecfc87cec39680bfcaea47fe3a54296d
pompomprin <jp53temple@icloud.com>
2018-12-10 01:28:25 +0900

    AirportActivityを作成。Intentのairportという名称にHNDかNRTが入ります。また、中央の画像と空港の画像は適当です。適宜変更をお願いします。

---
+++ app/src/main/java/com/websarva/wings/android/bestflightshot/AirportActivity.java
@@ -0,0 +1,27 @@
+package com.websarva.wings.android.bestflightshot;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class AirportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_airport);
+    }
+
+    public void onHanedaClick(View view){
+        Intent intent=new Intent(AirportActivity.this,SelectListActivity.class);
+        intent.putExtra("airport","HND");
+        startActivity(intent);
+    }
+
+    public void onNaritaClick(View view){
+        Intent intent=new Intent(AirportActivity.this,SelectListActivity.class);
+        intent.putExtra("airport","NRT");
+        startActivity(intent);
+    }
+}
