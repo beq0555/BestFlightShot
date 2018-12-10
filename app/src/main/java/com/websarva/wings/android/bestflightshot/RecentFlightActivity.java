a580b73e4972ba48ccfcf62762a6cb6b46aabf28
tkm <beq0555@mail3.doshisha.ac.jp>
2018-12-10 13:10:08 +0900

    検索方法を選択する画面のActivityを作成しました。ただし未完成です。すぐに完成させます。

--- app/src/main/java/com/websarva/wings/android/bestflightshot/RecentFlightActivity.java
+++ app/src/main/java/com/websarva/wings/android/bestflightshot/RecentFlightActivity.java
@@ -29,7 +29,7 @@ public class RecentFlightActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_recent_flight);
 
         //非同期処理を開始
         FlightInfoReceiver receiver = new FlightInfoReceiver();
--- app/src/main/java/com/websarva/wings/android/bestflightshot/SelectListActivity.java
+++ app/src/main/java/com/websarva/wings/android/bestflightshot/SelectListActivity.java
@@ -1,7 +1,17 @@
 package com.websarva.wings.android.bestflightshot;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SelectListActivity extends AppCompatActivity {
 
@@ -9,5 +19,59 @@ public class SelectListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_list);
+    }
+
+    private class MenuInfoReceiver extends AsyncTask<Void,String,String> {
+
+        @Override
+        public String doInBackground(Void... params) {
+
+            Intent intent = getIntent();
+            String airport = intent.getStringExtra("airport");
+            String urlStr = "https://api-tokyochallenge.odpt.org/api/v4/odpt:FlightInformationDeparture?acl:consumerKey=2af0930edd9f426efa146aa64e7d90d9b41b4fb84b9bef1e1040dce7e6fed3cf&odpt:departureAirport=odpt.Airport:" + airport;
+            String result = "";
+
+            HttpURLConnection con = null;
+            InputStream is = null;
+            try {
+                URL url = new URL(urlStr);
+                con = (HttpURLConnection) url.openConnection();
+                con.setRequestMethod("GET");
+                con.connect();
+                is = con.getInputStream();
+                result = is2String(is);
+            }catch (MalformedURLException ex) {
+                ex.printStackTrace();
+            }catch (IOException ex) {
+                ex.printStackTrace();
+            }finally {
+                if(con != null) {
+                    con.disconnect();
+                }
+                if(is != null) {
+                    try {
+                        is.close();
+                    }catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+            return result;
+        }
+        public void onPostExecute(String result) {
+
+            List<String> typeList = new ArrayList<>();
+            List<String> timeList = new ArrayList<>();
+            //flightInformationTextの内容を格納する配列
+            List<String> infoList = new ArrayList<>();
+
+            List<String> sortedTypeList = new ArrayList<>();
+            List<String> sortedTimeList = new ArrayList<>();
+            List<String> sortedInfoList = new ArrayList<>();
+
+
+
+
+        }
     }
 }
